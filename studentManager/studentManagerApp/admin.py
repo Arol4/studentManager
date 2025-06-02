d1ef0d3d255221b7476002e0ced98a7bcb5f6f06
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2025-06-02 19:57:17 +0100

    feat: add superuser and configure django administration

--- studentManager/studentManagerApp/admin.py
+++ studentManager/studentManagerApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Utilisateurs,Admins,Enseignants,Etudiants,Matieres,Evaluation,Notes
+admin.site.register(Utilisateurs)
+admin.site.register(Admins)
+admin.site.register(Enseignants)
+admin.site.register(Etudiants)
+admin.site.register(Matieres)
+admin.site.register(Evaluation)
+admin.site.register(Notes)
 
 # Register your models here.

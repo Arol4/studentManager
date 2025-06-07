b328f0d6164fe2084df2cf473735274614104e6b
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2025-06-07 04:59:00 +0100

    feat: configure displaying format in django administration

--- studentManager/studentManagerApp/models.py
+++ studentManager/studentManagerApp/models.py
@@ -13,12 +13,16 @@ class Admins(models.Model):
     prenom = models.CharField(max_length=20)
     poste = models.CharField(choices=choix_poste, default='concepteur')
     password = models.CharField(max_length=20,null=False)
+    def __str__(self):
+        return f"{self.nom} {self.prenom} - {self.poste}"
 
 class Enseignants(models.Model):
     enseignant_id=models.AutoField(primary_key=True)
     nom=models.CharField(max_length=20,null=False)
     prenom=models.CharField(max_length=20)
     password = models.CharField(max_length=20,null=False)
+    def __str__(self):
+        return f"{self.nom} {self.prenom}"
     
 class Etudiants(models.Model):
     etudiant_id=models.AutoField(primary_key=True)
@@ -27,6 +31,8 @@ class Etudiants(models.Model):
     prenom=models.CharField(max_length=20,null=False)
     dateNaiss=models.DateField(null=False)
     password = models.CharField(max_length=20,null=False)
+    def __str__(self):
+        return f"{self.nom} {self.prenom} - {self.matricule}"
 
 
 class Matieres(models.Model):
@@ -35,6 +41,8 @@ class Matieres(models.Model):
     semestre = models.IntegerField(null= False , validators= [MinValueValidator(1), MaxValueValidator(2)])
     nbre_credit = models.IntegerField()
     enseignant_id = models.ForeignKey(Enseignants, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.libelle} - Semestre {self.semestre} - {self.enseignant_id.nom} {self.enseignant_id.prenom}"
 
 
 class Evaluation(models.Model):
@@ -47,9 +55,13 @@ class Evaluation(models.Model):
     type_evaluation = models.CharField(choices= choix_type, default='controle_continu')
     date_evaluation = models.DateField(auto_now_add=True)
     Matiere_id = models.ForeignKey(Matieres, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.type_evaluation} - {self.date_evaluation} - {self.Matiere_id.libelle}"
 
 
 class Notes(models.Model):
     etudiant_id = models.ForeignKey(Etudiants, on_delete= models.CASCADE)
     evaluation_id = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
     notes = models.IntegerField(null= False, validators=[MinValueValidator(0), MaxValueValidator(20)])
+    def __str__(self):
+        return f"{self.etudiant_id.nom} {self.etudiant_id.prenom} - {self.evaluation_id.type_evaluation} - Note: {self.notes}"

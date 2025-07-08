da656afe42e15cad1018a61d5b3ab32ff8b41746
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2025-07-08 05:32:27 +0100

    fix: reintialization of the app

--- studentManager/studentManager/urls.py
+++ studentManager/studentManager/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from studentManagerApp import views
 urlpatterns = [
+    path('',views.login_view),
     path('admin/', admin.site.urls),
-    path('',views.home_view),
 ]
--- studentManager/studentManagerApp/views.py
+++ studentManager/studentManagerApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home_view(request):
+def login_view(request):
     return render(request,'studentManagerApp/log-in.html')
 
 # Create your views here.

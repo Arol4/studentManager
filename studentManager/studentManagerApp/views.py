57eda000bf6cf77b138570220e1e832c9d4d7f66
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2025-07-10 04:11:18 +0100

    fix: maintain the case combinaison name_password in many tables for views home and login

--- studentManager/studentManagerApp/views.py
+++ studentManager/studentManagerApp/views.py
@@ -11,23 +11,47 @@ def login_view(request):
             nom=form.cleaned_data['nom']
             password=form.cleaned_data['password']
             role=form.cleaned_data['role']
-            try:
-                id=Etudiant.objects.get(nom=nom,password=password).etudiant_id
-            except Etudiant.DoesNotExist:
+            if role == "etudiant":
+                try:
+                    id=Etudiant.objects.get(nom=nom,password=password).etudiant_id
+                except Etudiant.DoesNotExist:
+                        messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos étudiants")
+                        return redirect('login-page')
+            elif role == "enseignant":
                 try:
                     id=Enseignant.objects.get(nom=nom,password=password).enseignant_id
                 except Enseignant.DoesNotExist:
-                    try:
-                        id=Administrateur.objects.get(nom=nom,password=password).administrateur_id
-                    except Administrateur.DoesNotExist:
-                        messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos comptes")
+                        messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos enseigants")
                         return redirect('login-page')
+            else:
+                try:
+                    id=Administrateur.objects.get(nom=nom,password=password).administrateur_id
+                except Administrateur.DoesNotExist:
+                        messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos administrateurs")
+                        return redirect('login-page')               
             return redirect('home-page', id=id, role=role)
     else:
         form=LoginForm()
     return render(request,'studentManagerApp/log-in.html',{'form':form})
 
 def home_page_view(request, id, role):
-    user={'nom':"Berios",'role':role}
-    return render(request,'studentManagerApp/home-page.html',{'user':user})
+    if role == "etudiant":
+        try:
+            user=Etudiant.objects.get(etudiant_id=id)
+        except Etudiant.DoesNotExist:
+                messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos étudiants")
+                return redirect('login-page')
+    elif role == "enseignant":
+        try:
+            user=Enseignant.objects.get(enseignant_id=id)
+        except Enseignant.DoesNotExist:
+                messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos enseigants")
+                return redirect('login-page')
+    else:
+        try:
+            user=Administrateur.objects.get(administrateur_id=id)
+        except Administrateur.DoesNotExist:
+                messages.error(request,"Les donneés que vous avez entré ne correspondent á aucun de nos administrateurs")
+                return redirect('login-page')
+    return render(request,'studentManagerApp/home-page.html',{'user':user,'role':role})
 # Create your views here.
